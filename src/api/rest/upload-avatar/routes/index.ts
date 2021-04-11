e53a0942d006145ca8e5666e4effebac7b6d9d03
Rodrigo Magaldi <rodrigo.magaldi@taqtile.com.br>
2021-04-11 10:43:05 -0300

    Disable lint rule

--- src/api/rest/upload-avatar/routes/index.ts
+++ src/api/rest/upload-avatar/routes/index.ts
@@ -6,6 +6,7 @@ import { multerConfigs } from '../../config/multer-config';
 const uploadAvatarRouter = Router();
 const uploadAvatarController = new UploadAvatarController();
 
+// eslint-disable-next-line @typescript-eslint/unbound-method
 uploadAvatarRouter.post('/avatar', multerConfigs.single('avatar'), uploadAvatarController.upload);
 
 export default uploadAvatarRouter;
